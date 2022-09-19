78da5378a29744c6f75b03ea561b4d07240b6277
T-oluwadara <ajimotioluwadara@gmail.com>
2022-09-19 05:11:10 -0700

    changes

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const btn = document.getElementById('menu-btn');
-const nav = document.getElementById('menu');
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
+const dropdownElement = document.querySelectorAll(".dropdown");
 
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
 
-btn.addEventListener('click', () => {
-    btn.classList.toggle('open')
-    nav.classList.toggle('flex')
-    nav.classList.toggle('hidden')
+dropdownElement.forEach(function (element, key) {
+  element.onclick = function () {
+    element.classList.toggle("active");
+  };
+});
 
-})
+function aboutAccordion(tabId) {
+  document.querySelectorAll(".tab-content").forEach(function (element, key) {
+    element.style.display = "none";
+  });
+
+  document.getElementById(tabId).style.display = "block";
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       fontFamily: {
-        alata: ['Alata', 'sans- serif'],
-        jose: ['Josefin Sans', 'sans- serif'],
+        alata: ["Alata", "sans- serif"],
+        jose: ["Josefin Sans", "sans- serif"],
       },
       colors: {
-        white: 'hsl(0, 0%, 100%)',
-        black: 'hsl(0, 0%, 0%)',
-        darkGray: 'hsl(0, 0%, 55%)',
-        veryDarkGray: 'hsl(0, 0%, 41%)'
-
-      }
+        white: "hsl(0, 0%, 100%)",
+        black: "hsl(0, 0%, 0%)",
+        darkGray: "hsl(0, 0%, 55%)",
+        veryDarkGray: "hsl(0, 0%, 41%)",
+        almostBlack: "hsl(0, 0%, 12%)",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
